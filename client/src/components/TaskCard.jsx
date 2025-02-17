e6af94116cb7185b5f4fefb932c08ee3cc1756d3
ChristianScarabelli <scachmabol@hotmail.it>
2025-02-17 11:35:40 +0100

    ADD: handle priority color

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,15 +1,37 @@
 import DotsMenu from '../components/ui/DotsMenu.jsx'
 
-export default function TaskCard({ task, data }) {
+export default function TaskCard({ task }) {
 
     const { description, priority, completed } = task
+
+    // Funzione per switchare colore priority
+    const getPriorityColor = (priority) => {
+
+        if (priority == null) {
+            return 'bg-gray-200'
+        }
+
+        switch (priority) {
+            case 1:
+                return 'bg-green-600'
+            case 2:
+                return 'bg-orange-400'
+            case 3:
+                return 'bg-red-600'
+            default:
+                return 'bg-gray-200'
+        }
+    }
+
+    // Invoco la funzione e gli passo la priority che arriva dal DB
+    const priorityColor = getPriorityColor(priority)
+
     return (
-        <section className="flex flex-col p-3 bg-gray-200 rounded-lg text-gray-800">
-            <div className='flex justify-between items-center'>
-                <span>{priority}</span>
-                <DotsMenu className='text-gray-600 hover:bg-gray-600 hover:opacity-30 hover:text-white' />
+        <section className="flex flex-col bg-gray-200 rounded-lg text-gray-800">
+            <div className={`flex justify-end items-center px-3 py-1 ${priorityColor} rounded-t-lg`}>
+                <DotsMenu className='text-gray-700 hover:bg-gray-600 hover:opacity-30 hover:text-white' />
             </div>
-            <div>
+            <div className='p-3'>
                 <p>{description}</p>
                 <span>{completed}</span>
             </div>
--- client/src/components/ui/AddButton.jsx
+++ client/src/components/ui/AddButton.jsx
@@ -4,7 +4,7 @@ export default function AddButton({ onClick, data }) {
     return (
         <button
             onClick={onClick}
-            className="w-full flex items-center justify-start gap-2 p-3 rounded-lg text-gray-200 hover:bg-gray-600 hover:opacity-80"
+            className="cursor-pointer w-full flex items-center justify-start gap-2 p-3 rounded-lg text-gray-200 hover:bg-gray-600 hover:opacity-80"
         >
             <PlusIcon className="h-5 w-5" />
             <span className="text-sm font-medium">Add {data}</span>
